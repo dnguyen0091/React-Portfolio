cc8d498888e9b03b1f9a6a6229f27dcfad650e61
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-28 19:59:22 -0400

    functionality for buttons added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,12 +4,14 @@ import Resume from "../assets/ResumeDraft.pdf";
 
 export default function HomePage() {
 
-    const handleDownLoadCV = () => {
-        console.log("Download CV");
-    }
 
     const handleContactClick = () => {
         console.log("Contact");
+        const element = document.getElementById("contact");
+        if (element)
+        {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
     }
     return(
         <div className="h-screen">
@@ -18,8 +20,8 @@ export default function HomePage() {
                 <p className="text-[40px]">I am a passionate Software Engineer.</p>
                 <div className="flex flex-row gap-10 mt-10">
                     
-                    <a href={Resume} download="DavidNguyenCV"><button className="border-[var(--button-secondary)] border-2 rounded-xl w-[5vw] bg-[var(--button-primary)] text-[var(--button-text)] p-[0.75rem]" onClick={handleDownLoadCV()}>Download CV</button></a>
-                    <button className="border-[var(--button-secondary)] border-2 rounded-xl w-[5vw] bg-[var(--button-primary)] text-[var(--button-text)] p-[0.75rem]" onClick={handleContactClick()}>Contact Me</button>
+                    <a href={Resume} download="DavidNguyenCV"><button className="border-[var(--button-secondary)] border-2 rounded-xl w-[5vw] bg-[var(--button-primary)] text-[var(--button-text)] p-[0.75rem]">Download CV</button></a>
+                    <button className="border-[var(--button-secondary)] border-2 rounded-xl w-[5vw] bg-[var(--button-primary)] text-[var(--button-text)] p-[0.75rem]" onClick={handleContactClick}>Contact Me</button>
                 </div>
             </div>
         </div>
