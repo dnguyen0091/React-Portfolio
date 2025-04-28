64ad32c3216d549210b58fdfc172988cdf955670
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-28 19:38:45 -0400

    Styled navigation bar

--- src/Components/navigationBar.jsx
+++ src/Components/navigationBar.jsx
@@ -2,7 +2,9 @@ import { useEffect, useRef, useState } from 'react';
 import emailIcon from '../assets/Icons/emailIcon.svg';
 import gitHubIcon from '../assets/Icons/githubIcon.svg';
 import linkedInIcon from '../assets/Icons/linkedInIcon.svg';
+import '../index.css';
 import toggleThemes from './toggleThemes';
+
 
 export default function NavigationBar() {
     const [navOpen, setNavOpen] = useState(false);
@@ -74,13 +76,13 @@ export default function NavigationBar() {
         navigator.clipboard.writeText("davidnguyen0091@icloud.com");
     }
     return(
-        <div className="flex flex-row justify-between items-center h-[5vh] z-1000 absolute top-5 left-[40vw] gap-[5vw] border-black border-2 rounded-[40px] bg-white p-5">
-            <div className="dropDown relative" ref={navRef} onClick={handleNavigateClick}>
+        <div className="flex flex-row justify-between items-center w-[20vw] h-[5vh] z-1000 top-5 left-[40vw] gap-[5vw] border-[var(--tertiary)] border-2 rounded-[40px] bg-[var(--secondary)] p-5 sticky">
+            <div className="dropDown relative cursor-pointer" ref={navRef} onClick={handleNavigateClick}>
                 Navigate
 
                 <div className={`navigateDropDown absolute top-full left-0 mt-4 gap-[2vh] ${navOpen ? 'block' : 'hidden'}`}>
                     <div>
-                        <div className="dropItems flex flex-col gap-[2vh] mt-3">
+                        <div className="dropItems flex flex-col gap-[2vh] mt-3 p-2 rounded-lg bg-[var(--secondary)] text-[var(--text-secondary)]">
                             <div onClick={() => handleSectionClick('home')}>Landing Page</div>
                             <div onClick={() => handleSectionClick('about')}>About</div>
                             <div onClick={() => handleSectionClick('experience')}>Experience</div>
@@ -90,11 +92,11 @@ export default function NavigationBar() {
                     </div>
                 </div>
             </div>
-            <div className="dropDown relative" ref={socialsRef} onClick={handleSocialsClick}>
+            <div className="dropDown relative cursor-pointer" ref={socialsRef} onClick={handleSocialsClick}>
                 Socials
 
                 <div className={`socialsDropDown absolute top-full left-0 mt-4 ${socialsOpen ? 'block' : 'hidden'}`}>
-                    <div className="dropItems flex flex-col gap-[2vh] mt-3">
+                    <div className="dropItems flex flex-col w-[12vw] gap-[2vh] mt-3 p-2 rounded-lg bg-[var(--secondary)] text-[var(--text-secondary)]">
                         <a className="flex flex-row items-center gap-1" href="https://www.linkedin.com/in/dnguyen0091/"><img className="w-[1.5vw]" src={linkedInIcon} alt="LinkedIn Icon" />LinkedIn</a>
                         <a className="flex flex-row items-center gap-1" href="https://github.com/dnguyen0091"><img className="w-[1.5vw]" src={gitHubIcon} alt="GitHub Icon" />GitHub</a>
                         <div className="flex flex-row items-center gap-1" onClick={handleCopyEmail}><img className="w-[1.5vw]" src={emailIcon} alt="Email Icon" />davidnguyen0091@icloud.com</div>
@@ -103,7 +105,7 @@ export default function NavigationBar() {
                 </div>
             </div>
             
-            <div onClick={handleColorClick}>
+            <div className="cursor-pointer" onClick={handleColorClick}>
                 Color
             </div>
         </div>
