c63b9a92f225a9c7a7a9ceb03ee3308fcda6db0c
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-29 19:58:00 -0400

    basic styling for the contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,16 +1,27 @@
-
+import '../index.css';
 
 export default function Contact()
 {
     return(
-        <div className="flex flex-col items-center justify-center">
-            <p className="text-[40px]">Contact</p>
+        <div className="flex flex-col items-center justify-center h-[50vh] gap-[2.5em] text-[var(--text-primary)]">
+            <p className="text-[60px]">Contact Me</p>
 
             <form action="">
-                <div className="flex flex-col">
-                    <input type="text" placeholder="Name"/>
-                    <input type="text" placeholder="Email"/>
-                    <input type="text" placeholder="Message"/>
+                <div className="flex flex-col gap-[2.5em]">
+                    <div>
+                        <p className="text-[20px]">Name</p>
+                        <input type="text" placeholder="Ex. John Doe" className="h-[3vh] w-[15vw] p-2 border-[var0(--input-border)] border-[0.1rem] rounded-md bg-[var(--input-bg)]"/>
+                    </div>
+                    
+                    <div>
+                        <p className="text-[20px]">Email</p>
+                        <input type="text" placeholder="Ex. example@example.org" className="h-[3vh] w-[15vw] p-2 border-[var0(--input-border)] border-[0.1rem] rounded-md bg-[var(--input-bg)]"/>
+                    </div>
+                    <div>
+                        <p className="text-[20px]">Message</p>
+                        <input type="text" placeholder="Message" className="h-[3vh] w-[15vw] p-2 border-[var0(--input-border)] border-[0.1rem] rounded-md bg-[var(--input-bg)]"/>
+                    </div>
+                    <button type="submit" className="bg-[var(--button-primary)] text-[var(--button-text)] h-[3vh]">Submit</button>
                 </div>
             </form>
         </div>
