a67913534888b863fa19a0b89ed584f9ec9e92bc
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-30 11:31:12 -0400

    initial skeleton for experience page

--- src/Pages/WorkExp.jsx
+++ src/Pages/WorkExp.jsx
@@ -4,6 +4,33 @@ export default function WorkExp(){
     return(
         <div className="flex flex-col items-center justify-center h-[50vh] gap-[2.5em] text-[var(--text-primary)]">
             <p className="mt-[10vh] text-[40px]">Experience</p>
+            <div>
+                <p>Lockheed Martin</p>
+                <p>CWEP Software Engineer/May 2025-Present</p>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div>
+                <p>Wicked Bao LLC</p>
+                <p>Kitchen Expo & Customer Service/June 2020-May 2025</p>
+                <ul>
+                    <li><p>Supported dining operations as a high school sophomore, bussing tables and delivering meals to customers.</p></li>
+
+                    <li><p>Advanced to a managerial role, ensuring order accuracy, training new hires, and coordinating kitchen workflows.</p></li>
+
+                    <li><p>Maintained consistent seasonal employment for four years, contributing to smooth operations during high-demand periods</p></li>
+                </ul>
+            </div>
+
+            <div>
+                <p>Publix Supermarkets</p>
+                <p>Customer Service/Feb. 2019-June 2020</p>
+                <ul>
+                    <li><p>Worked collaboratively with cashiers and other team members to maintain a smooth checkout process</p></li>
+                </ul>
+            </div>
         </div>
     )
 }
