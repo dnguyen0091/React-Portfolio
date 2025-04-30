60b1acee475892e2318a79c9f1cb4711229741b7
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-29 21:32:06 -0400

    initialized final page layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import './index.css'
 import AboutPage from './Pages/AboutPage'
 import Contact from './Pages/Contact'
 import HomePage from './Pages/HomePage'
+import Projects from './Pages/Projects'
+import Experience from './Pages/WorkExp'
 function App() {
   return (
     <>
       <NavigationBar />
       <div id="home"><HomePage /></div>
       <div id="about"><AboutPage /></div>
-      <div id="experience">{/* Experience component */}</div>
-      <div id="projects">{/* Projects component */}</div>
+      <div id="experience"><Experience/></div>
+      <div id="projects"><Projects/></div>
       <div id="contact"><Contact /></div>
     </>
   )
