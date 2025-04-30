90dd1fb1447cf8007dba380d799ceb91e11f2d39
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-30 13:18:45 -0400

    Initial skeleton for tech stack page

---
+++ src/Pages/TechStack.jsx
@@ -0,0 +1,31 @@
+import expressIcon from '../assets/Tech Stack/expressjs.png'
+import gitIcon from '../assets/Tech Stack/git.png'
+import mongoDBIcon from '../assets/Tech Stack/mongodb.png'
+import nextIcon from '../assets/Tech Stack/nextjs.png'
+import nodeIcon from '../assets/Tech Stack/nodejs.png'
+import pythonIcon from '../assets/Tech Stack/python.png'
+import reactIcon from '../assets/Tech Stack/reactjs.png'
+import tailwindIcon from '../assets/Tech Stack/tailwindcss.png'
+
+
+export default function TechStack()
+{
+    return(
+        <div className="flex flex-col justify-center items-center">
+            <p className="mt-[10vh] text-[40px]">Current Tech Stack</p>
+
+            <div className="grid grid-cols-4 grid-rows-2 gap-[3vw] mt-8">
+                <div className="flex flex-col items-center gap-5"><img src={reactIcon} alt="React" className="w-[5vw]"/><p className="text-[20px]">React</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={mongoDBIcon} alt="MongoDB" className="w-[5vw]"/><p className="text-[20px]">MongoDB</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={nodeIcon} alt="Node.js" className="w-[5vw]"/><p className="text-[20px]">Node.js</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={expressIcon} alt="Express.js" className="w-[5vw]"/><p className="text-[20px]">Express.js</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={nextIcon} alt="Next.js" className="w-[5vw]"/><p className="text-[20px]">Next.js</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={tailwindIcon} alt="Tailwind" className="w-[5vw]"/><p className="text-[20px]">Tailwind.css</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={gitIcon} alt="git" className="w-[5vw]"/><p className="text-[20px]">Git</p></div>
+                <div className="flex flex-col items-center gap-5"><img src={pythonIcon} alt="Python" className="w-[5vw]"/><p className="text-[20px]">Python</p></div>
+                
+            </div>
+            
+        </div>
+    )
+}
