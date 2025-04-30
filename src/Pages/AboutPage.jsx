9db6a368e86c9c60dca5b091e9b3fe7f16aa2223
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-30 15:31:25 -0400

    About me message

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -8,8 +8,14 @@ export default function AboutPage()
             <div className="flex flex-col justify-center items-center">
                 <p className="mt-[10vh] text-[40px]">About</p>
                 <div className="flex flex-row items-center justify-center mt-[40vh] gap-[10vw]">
+                        
+                        <p>
+                            I picked up Web Development back in 2022 as a way to learn more about coding. From there I fully delved into the rabbit hole of the world of software engineering.
+                            I discovered my passion for learning and the creation, which led me to pursue a career in this field. I am currently a student at the Unviversity of Central Florida, where I am studying Computer Science.
+                            Outside of school and work, you can find me working on a personal project, enjoying games, at the gym, or spending time with my friends and family.
+                            I am always looking for new opportunities to learn and grow, so if you have any questions or would like to connect, feel free to reach out!
+                        </p>
                         <img src="" alt="Placeholder For Headshot" />
-                        <p>Info About Me</p>
                 </div>
             </div>
         </div>
