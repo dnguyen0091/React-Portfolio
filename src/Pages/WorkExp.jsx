a8bbcc73f9a13860a035ca7b091911079e4813ed
dnguyen0091 <davidnguyen0091@icloud.com>
2025-04-29 21:32:55 -0400

    Initialized section for work experience

---
+++ src/Pages/WorkExp.jsx
@@ -0,0 +1,9 @@
+
+
+export default function WorkExp(){
+    return(
+        <div className="flex flex-col items-center justify-center h-[50vh] gap-[2.5em] text-[var(--text-primary)]">
+            <p className="mt-[10vh] text-[40px]">Experience</p>
+        </div>
+    )
+}
